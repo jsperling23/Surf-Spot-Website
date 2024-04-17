5b65198a2605f3f0cc9b67efc0b96e6976753942
jsperling23 <95095370+jsperling23@users.noreply.github.com>
2024-04-16 18:24:41 -0700

    Delete database directory

--- database/dml.sql
+++
@@ -1,7 +0,0 @@
---Get buoys between a specific latitude and longitude range
-SELECT * FROM Buoys
-WHERE 
-    (latitude BETWEEN :lower AND :upper)
-    AND
-    (longitude BETWEEN :lower AND :upper)
-ORDER BY latitude DESC;
